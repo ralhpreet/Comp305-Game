ce03735163d85ef017a388f926945b2f5f893ad8
ralhpreet <preetralh07@gmail.com>
2016-02-04 18:24:06 -0500

    collision + audio on collision with the audio provided by professor

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+    //Public instance variables
+    public int fireBallNumber=30;
+    public FireBallController  fireBalls;
+
+	// Use this for initialization
+	void Start () {
+        this._initialize();
+        //fireBalls.gameObject;
+        //f
+	}
+	
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //Private Methods###########
+    private void _initialize(){
+        //this.fireBallNumber = 3;
+        for (int fireBallCount = 0; fireBallCount < this.fireBallNumber; fireBallCount++)
+        {
+            Instantiate(fireBalls.gameObject);
+        }
+    }
+}
---
+++ Assets/_Scripts/IronmanCollider.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IronmanCollider : MonoBehaviour {
+    //Private Instance Variables
+    private AudioSource[] _audioSources;
+    private AudioSource _fireBallSound;
+    private AudioSource _heartSound;
+
+	// Use this for initialization
+	void Start () {
+        //Initialize the audioSources array
+        this._audioSources = gameObject.GetComponents<AudioSource>();
+        this._fireBallSound = this._audioSources[1];
+        this._heartSound = this._audioSources[2];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnTriggerEnter2D(Collider2D ogo)
+    {
+        if (ogo.gameObject.CompareTag("FireBall"))
+        {
+            this._fireBallSound.Play();
+        }
+        if (ogo.gameObject.CompareTag("Heart"))
+        {
+            this._heartSound.Play();
+        }
+    }
+}
