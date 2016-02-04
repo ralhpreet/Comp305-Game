fb4aecf855ecf781a3a37c0846db345280122fde
ralhpreet <preetralh07@gmail.com>
2016-02-04 03:52:26 -0500

    added fireball with verticaldriftspeed and horizontalspeed plus a prefab

--- Assets/_Scripts/FireBallController.cs
+++ Assets/_Scripts/FireBallController.cs
@@ -3,13 +3,46 @@ using System.Collections;
 
 public class FireBallController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    //Public instance variables
+    
+    public float minHorizontalSpeed = 5f;
+    public float maxHorizontalSpeed = 10f;
+    public float minVerticalSpeed = -2f;
+    public float maxVerticalSpeed = 2f;
+    
+    
+    //Private Instace Variables
+    private Vector2 _currentPosition;
+    private Transform _transform;
+    private float _horizontalSpeed;
+    private float _verticalDrift;
+   
+    
+    // Use this for initialization
+    void Start()
+    {
+        //Make a reference with transform component
+        this._transform = gameObject.GetComponent<Transform>();
+        //Reset the FireBall sprite to the top
+        this.Reset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this._currentPosition = this._transform.position;
+        this._currentPosition -= new Vector2(this._horizontalSpeed, this._verticalDrift);
+        this._transform.position = this._currentPosition;
+        if (this._currentPosition.x <= -300)
+        {
+            this.Reset();
+        }
+    }
+    void Reset()
+    {
+        this._horizontalSpeed = Random.Range(this.minHorizontalSpeed, this.maxHorizontalSpeed);
+        this._verticalDrift = Random.Range(this.minVerticalSpeed, this.maxVerticalSpeed);
+        float yPosition = Random.Range(-160f, 160f);
+        this._transform.position = new Vector2(320f, yPosition);
+    }
 }
---
+++ Assets/_Scripts/HeartController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeartController : MonoBehaviour {
+
+    //Public instance variables
+    public float speed = 5f;
+    //Private Instace Variables
+    private Vector2 _currentPosition;
+    Transform _transform;
+    // Use this for initialization
+    void Start()
+    {
+        //Make a reference with transform component
+        this._transform = gameObject.GetComponent<Transform>();
+        //Reset the Heart sprite to the top
+        this.Reset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this._currentPosition = this._transform.position;
+        this._currentPosition -= new Vector2(this.speed, 0);
+        this._transform.position = this._currentPosition;
+        if (this._currentPosition.x <= -260)
+        {
+            this.Reset();
+        }
+    }
+    void Reset()
+    {
+        float yPosition = Random.Range(-150f, 145f);
+        this._transform.position = new Vector2(260f, yPosition);
+    }
+}
